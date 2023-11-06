ebdd81a3984a6e71f598aea7b66ccbd181e878f3
Jansen Muniz <jansen.muniz1@gmail.com>
2023-11-06 11:03:37 -0300

    Componentização

--- src/App.jsx
+++ src/App.jsx
@@ -14,43 +14,59 @@ const steps = [
     description: "Repetir até o cliente ficar feliz e encher seu 🍑 de dinheiro",
   },
 ]
+const Steps = ({ shouldBeOpen, step, onClickNext, onClickPrevious }) =>
+  shouldBeOpen && (
+    <>
+      <div className="steps">
+        <ul className="numbers">
+          {steps.map((item, i) => (
+            <li key={item.id} className={i + 1 === step ? 'active' : ''}>
+              {i + 1}
+            </li>
+          ))}
+        </ul>
+
+        <h3 className="message">
+          Passo {step}: {steps[step - 1].description}
+        </h3>
+
+        <div className="buttons">
+          <button onClick={onClickPrevious}>Anterior</button>
+          <button onClick={onClickNext}>Próximo</button>
+        </div>
+      </div>
+    </>
+  )
+
+const Toggle = ({ shouldBeOpen, onClickToggle }) => (
+  <div className="container-close">
+    <button className="close" onClick={onClickToggle}>
+      {shouldBeOpen ? 'Fechar' : 'Abrir'}
+    </button>
+  </div>
+)
 
 const App = () => {
   const [shouldBeOpen, setShouldBeOpen] = useState(true)
   const [step, setStep] = useState(1)
 
-  const handleClickToogle = () => setShouldBeOpen(s => !s)
-  const handlePrevStep = () => setStep(s => s - 1 === 0 ? s : s - 1)
-  const handleNextStep = () => setStep(s => s === steps.length ? s : s + 1)
+  const handleClickToggle = () => setShouldBeOpen(s => !s)
+  const handleClickNext = () => setStep(s => s === steps.length ? s : s + 1)
+  const handleClickPrevious = () => setStep(s => s - 1 === 0 ? s : s - 1)
 
   return (
     <>
-      <div className="container-close">
-        <button className="close" onClick={handleClickToogle}>
-          {shouldBeOpen ? 'Fechar' : 'Abrir'}
-        </button>
-      </div>
+      <Toggle
+        shouldBeOpen={shouldBeOpen}
+        onClickToggle={handleClickToggle}
+      />
 
-      {shouldBeOpen && (
-        <div className="steps">
-          <ul className="numbers">
-            {steps.map((item, i) => (
-              <li key={item.id} className={i + 1 === step ? 'active' : ''}>
-                {i + 1}
-              </li>
-            ))}
-          </ul>
-
-          <h3 className="message">
-            Passo {step} : {steps[step - 1].description}
-          </h3>
-
-          <div className="buttons">
-            <button onClick={handlePrevStep}>Anterior</button>
-            <button onClick={handleNextStep}>Próximo</button>
-          </div>
-        </div>
-      )}
+      <Steps
+        shouldBeOpen={shouldBeOpen}
+        step={step}
+        onClickPrevious={handleClickPrevious}
+        onClickNext={handleClickNext}
+      />
     </>
   )
 }
